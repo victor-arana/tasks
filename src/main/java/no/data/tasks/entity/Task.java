ff9e838f37384a524943bc8136590012c4ecda40
victor.arana <victor.arana@walmart.com>
2020-07-11 21:03:14 -0500

    :building_construction: persistence, maps class to database

--- src/main/java/no/data/tasks/entity/Task.java
+++ src/main/java/no/data/tasks/entity/Task.java
@@ -5,14 +5,29 @@ import lombok.Setter;
 import lombok.ToString;
 import no.data.tasks.entity.enums.TaskCategory;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Enumerated;
+import javax.persistence.EnumType;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @ToString
+@Entity
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
     private LocalDateTime start;
+
     private LocalDateTime finish;
+
+    @Enumerated(EnumType.STRING)
     private TaskCategory taskCategory;
+
     private String description;
 }
