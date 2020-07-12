ad91e024240bfcc5370d7c1122e1c137c6bc8118
victor.arana <victor.arana@walmart.com>
2020-07-11 20:29:15 -0500

    :building_construction: feature, return list of tasks instead of string

--- src/main/java/no/data/tasks/controller/TasksController.java
+++ src/main/java/no/data/tasks/controller/TasksController.java
@@ -1,13 +1,26 @@
 package no.data.tasks.controller;
 
+import no.data.tasks.entity.Task;
+import org.apache.tomcat.jni.Local;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class TasksController {
 
     @GetMapping("/tasks/")
-    public String getTasks(){
-        return "tasks";
+    public List<Task> getTasks(){
+        Task firstTask = new Task();
+        firstTask.setStart(LocalDateTime.now());
+        firstTask.setStart(LocalDateTime.now().plusMinutes(30L));
+
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(firstTask);
+
+        return tasks;
     }
 }
