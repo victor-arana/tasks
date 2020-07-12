c9cba3930a63670c37544deb4a06485942dfc5d9
victor.arana <victor.arana@walmart.com>
2020-07-11 21:35:38 -0500

    :building_construction: persistence, use repository

--- src/main/java/no/data/tasks/controller/TasksController.java
+++ src/main/java/no/data/tasks/controller/TasksController.java
@@ -1,26 +1,22 @@
 package no.data.tasks.controller;
 
+import no.data.tasks.dao.TasksRepository;
 import no.data.tasks.entity.Task;
-import org.apache.tomcat.jni.Local;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class TasksController {
 
+    @Autowired
+    private TasksRepository repository;
+
     @GetMapping("/tasks/")
     public List<Task> getTasks(){
-        Task firstTask = new Task();
-        firstTask.setStart(LocalDateTime.now());
-        firstTask.setStart(LocalDateTime.now().plusMinutes(30L));
-
-        List<Task> tasks = new ArrayList<>();
-        tasks.add(firstTask);
-
+        List<Task> tasks = repository.findAll();
         return tasks;
     }
 }
